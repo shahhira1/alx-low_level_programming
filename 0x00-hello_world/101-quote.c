f6f6cc3b42087296b39bb9e0ea703242b90c41ae
shahhira1 <shahira.ahmed2@gmail.com>
2023-06-15 18:41:19 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints out the last part of a quote in the standard error.
+ * main - A c program that prints a line to standard error.
  * Return: 1 if success.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	fput("and that piece of art is useful - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
