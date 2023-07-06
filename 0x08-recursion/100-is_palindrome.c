442343346df3e8a9fb2659c79edbee72e845cfa8
shahhira1 <shahira.ahmed2@gmail.com>
2023-07-05 20:09:12 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,45 +1,62 @@
 #include "main.h"
 
-int check_pal(char *s, int i, int len);
-int _strlen_recursion(char *s);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
 
 /**
-* is_palindrome - checks if a string is a palindrome
-* @s: string to reverse
-*
-* Return: 1 if it is, it's not
-*/
+ * find_strlen - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+
+	return (len);
+}
+
+/**
+ * check_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * @len: The length of s.
+ * @index: The index of the string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
+ */
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
+ */
 int is_palindrome(char *s)
 {
-if (*s == 0)
-return (1);
-return (check_pal(s, 0, _strlen_recursion(s)));
-}
-/**
-* _strlen_recursion - returns the length of a string
-* @s: string to calculate the length of
-*
-* Return: length of the string
-*/
-int _strlen_recursion(char *s)
-{
-if (*s == '\0')
-return (0);
-return (1 + _strlen_recursion(s + 1));
-}
-/**
- * check_pal - checks the characters recursively for palindrome
- * @s: string to check
- * @i: iterator
- * @len: length of the string
- *
- * Return: 1 if palindrome, 0 if not
- */
-int check_pal(char *s, int i, int len)
-{
-if (*(s + 1) != *(s + len - 1))
-return (0);
-if (i >= len)
-return (1);
-return (check_pal(s, i + 1, len - 1));
+	int index = 0;
+	int len = find_strlen(s);
+
+	if (!(*s))
+		return (1);
+
+	return (check_palindrome(s, len, index));
 }
