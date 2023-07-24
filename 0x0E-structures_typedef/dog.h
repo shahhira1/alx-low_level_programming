b7b7850db372546b8b7fa643296f45aa259fa91e
shahhira1 <shahira.ahmed2@gmail.com>
2023-07-23 19:14:35 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,13 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
+
 };
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
