dec73b7fa9fda94a69b705669c649132c3a358e9
shahhira1 <shahira.ahmed2@gmail.com>
2023-10-30 09:35:31 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
-include "main.h"
+#include "main.h"
+
 /**
 * create_file - Creates a file with given text content
 * @filename: The name of the file to create
@@ -6,14 +7,14 @@ include "main.h"
 * Return: 1 on success, -1 on failure
 */
 
-int create file(const char *filename, char *text_content)
-
+int create_file(const char *filename, char *text_content);
 {
+
 int file, result = 1, countn = 0;
 
 if (!filename)
 return (-1);
-file = open(filename, 0_CREAT | O_WRONLY | O_TRUNC, 0600);
+file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (file == -1)
 return (-1);
 if (text_content)
