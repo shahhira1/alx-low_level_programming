2fa3a469f996897b621acd0f64c119fed09d8ab1
shahhira1 <shahira.ahmed2@gmail.com>
2023-10-30 09:47:47 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,26 +6,24 @@
 * @text_content: A NULL-terminated string to write to the file
 * Return: 1 on success, -1 on failure
 */
+int create_file(const char *filename, char *text_content)
 
-int create_file(const char *filename, char *text_content);
 {
-
-int file, result = 1, countn = 0;
-
-if (!filename)
-return (-1);
-file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (file == -1)
-return (-1);
-if (text_content)
-{
-while (text_content[countn])
-countn++;
-result = write(file, text_content, countn);
+	int file, result = 1, countn = 0;
+	if (!filename)
+		return (-1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+	
+	if (text_content)
+	{
+		while (text_content[countn])
+			countn++;
+		result = write(file, text_content, countn);
+	}
+	if (result == -1)
+	return (-1);
+	close(file),
+		return (1);
 }
-if (result == -1)
-return (-1);
-close(file),
-return (1);
-}
-
